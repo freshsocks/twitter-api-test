0d4f0dc31a16a059991ffaddbbf3bbce06ce38bb
stevefloat <steve@floattothetop.com>
2013-10-02 19:19:36 -0400

    SIO working. Trying to use sockets for twitter api call.

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,12 @@
-window.onload = function() {
+$( document ).ready(function() {
+
 	var socket = io.connect('http://54.221.206.170');
 	socket.on('news', function (data) {
 		console.log(data);
 		socket.emit('my other event', { my: 'data' });
 	});
-}
+	socket.on('twitter-data-update', function (data) {
+		console.log(data);
+	});
+
+});
