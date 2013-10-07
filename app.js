fad08fd9d6ad7eff7660a1c72c96a924a490e223
stevefloat <steve@floattothetop.com>
2013-10-07 09:24:04 -0400

    Some minor structural updates

--- app.js
+++ app.js
@@ -5,25 +5,28 @@
 
 var DEV_PORT = 3000;
 
-var routes = require('./routes');
-var user = require('./routes/user');
-var path = require('path');
-var util = require('util');
-var twitter = require('twitter');
-var twit = new twitter({
-    consumer_key: 'mYVqwQEonkbBww11sMg',
-    consumer_secret: 'BVRyXDr7PxPkwh74cM0K8DTQaxmiNCvNGdQ80mCE8',
-    access_token_key: '86416702-duXu6pIFjFzxq2yN2V71jsydxXo10hJhOlxCw1mIJ',
-    access_token_secret: 'w6VC6eBJUsmRn51mZk5n0hj1A4Dd6dS4FMVo1lKNA'
-});
+var routes = require('./routes')
+  , path = require('path')
+  , util = require('util')
+  , _ = require('underscore')
+  , twitter = require('twitter')
+  , twit = new twitter({
+    	consumer_key: 'mYVqwQEonkbBww11sMg',
+    	consumer_secret: 'BVRyXDr7PxPkwh74cM0K8DTQaxmiNCvNGdQ80mCE8',
+    	access_token_key: '86416702-duXu6pIFjFzxq2yN2V71jsydxXo10hJhOlxCw1mIJ',
+    	access_token_secret: 'w6VC6eBJUsmRn51mZk5n0hj1A4Dd6dS4FMVo1lKNA'
+	});
 
-//var _ = require('underscore');
 
-// Init server, express app, and socket. Everyone is listening.
+/**
+ * Create app, server, and socket.io
+ */
+
 var express = require('express')
-	, app = express()
-	, server = require('http').createServer(app)
-	, io = require('socket.io').listen(server);
+  , app = express()
+  , server = require('http').createServer(app)
+  , io = require('socket.io').listen(server);
+
 
 // all environments
 app.set('port', process.env.PORT || DEV_PORT);
@@ -39,19 +42,35 @@ app.use(app.router);
 app.use(require('stylus').middleware(__dirname + '/public'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
+app.configure('development', function(){
+	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
 
-server.listen(app.get('port'), function(){
-	console.log('Express server listening on port ' + app.get('port'));
+app.configure('production', function(){
+	app.use(express.errorHandler());
+	app.use(express.compress());
 });
 
 app.get('/', routes.index);
-app.get('/users', user.list);
+
+io.configure(function() {
+	io.enable('browser client minification');
+	io.enable('browser client etag');
+	io.enable('browser client gzip');
+	io.set('heartbeat interval', 60);
+	io.set('heartbeat timeout', 120);
+	io.set('log level',1);
+	io.set('transports',[
+		'websocket',
+		'flashsocket',
+		'htmlfile',
+		'xhr-polling',
+		'jsonp-polling'
+	]);
+});
 
 io.sockets.on('connection', function (socket) {
+	
 	console.log('HANDSHAKE MADE!');
 
 	socket.on('get-twitter-stream', function (username){
@@ -66,5 +85,18 @@ io.sockets.on('connection', function (socket) {
 				console.log("twit.get > tweets\n"+util.inspect(tweets));
 				socket.emit('twitter-data-update', tweets );
 		});
+	});
+});
+
+
+/**	///////////////////////////////////////////////////////////
+ *  ///														///
+ * 	///		Launch the server after everything is set up.	///
+ *  ///														///
+ */ ///////////////////////////////////////////////////////////
+
+_.defer(function() {
+	server.listen(app.get('port'), function(){
+		console.log('Express server listening on port ' + app.get('port'));
 	});
 });
